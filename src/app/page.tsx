db17e3efae161b6905be50141e553dfa629989d3
gabrielynw <gabrielsousadeveloper@gmail.com>
2024-05-29 11:23:06 -0300

    feat: adicionando componente características ao layout da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Banner } from '@/components/Banner/banner';
+import { Caracteristicas } from '@/components/Caracteristicas/index';
 import { Flex } from '@chakra-ui/react';
 
 export default function Home() {
     return (
         <Flex direction='column'>
             <Banner />
+            <Caracteristicas />
         </Flex>
     );
 }
