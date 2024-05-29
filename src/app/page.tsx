16e6567e6ce7c59376b85a4d92feb98b9156b2ff
gabrielynw <gabrielsousadeveloper@gmail.com>
2024-05-29 10:35:11 -0300

    adicionando componente banner ao layout da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Banner } from '@/components/Banner/banner';
 import { Flex } from '@chakra-ui/react';
 
 export default function Home() {
-    return <Flex direction='column'></Flex>;
+    return (
+        <Flex direction='column'>
+            <Banner />
+        </Flex>
+    );
 }
